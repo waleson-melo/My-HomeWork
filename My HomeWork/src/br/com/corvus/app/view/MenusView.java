03cdcda614f441f2c33102870db38f93c79620e8
Waleson Melo <walesonmelo23@gmail.com>
2021-09-20 16:10:47 -0400

    Create MenusView.java
    Nessa classe vai todos os menus do programa

---
+++ My HomeWork/src/br/com/corvus/app/view/MenusView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.corvus.app.view;
+
+/**
+ *
+ * @author waleson_melo
+ */
+public class MenusView {
+    public void menuPrincipal(){
+        System.out.println("===============================");
+        System.out.println("=======|  MY HOMEWORK  |=======");
+        System.out.println("===============================");
+
+        System.out.println("(1) Gerenciar Atividades");
+        System.out.println("(2) Gerenciar Materias");
+        System.out.println("(3) Gerenciar Insituições");
+        System.out.println("(4) Gerenciar Professores");
+        System.out.println("(5) Gerenciar Meus Dados");
+        System.out.println("(0) Sair do Programa");
+        System.out.println("Opção: ");
+    }
+}
