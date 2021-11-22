227d42202c8421a4494877ba8a9347d35f86b8c1
Maryse Pires <74427494+marysepires@users.noreply.github.com>
2021-11-21 21:54:23 -0300

    .

--- My HomeWork/src/br/com/corvus/app/model/AlunoModel.java
+++ My HomeWork/src/br/com/corvus/app/model/AlunoModel.java
@@ -7,8 +7,27 @@ package br.com.corvus.app.model;
 
 /**
  *
- * @author waleson_melo
+ * @author caroi
  */
 public class AlunoModel {
+    
+    private int matrícula;
+    private String email;
+   
+    public int getMatricula(){
+        return matrícula;
+    }
+    
+    public void setMatricula( int matrícula ){
+        this.matrícula = matrícula;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail( String email ){
+        this.email = email;
+    }
     
 }
